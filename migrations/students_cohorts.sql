033de3bd4d70dd586a89d07f377fe942e36b7916
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-07-28 05:26:12 +0000

    set up project, create a database bootcampx, create table students and cohorts, add attributes

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,17 @@
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL, 
+  start_date DATE, 
+  end_date DATE
+);
+
+CREATE TABLE students ( 
+  id SERIAL PRIMARY KEY NOT NULL, 
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE,
+  name TEXT NOT NULL, 
+  email TEXT, 
+  phone TEXT, 
+  github TEXT, 
+  start_date DATE, 
+  end_date DATE
+  );
